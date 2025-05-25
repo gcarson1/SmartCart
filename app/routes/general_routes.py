db40b2af6ee57f0ccf8dbe3bf908cc5eecbdd742
gcarson1 <gcarson1@vols.utk.edu>
2025-05-24 22:59:52 -0400

    stuff

--- app/routes/general_routes.py
+++ app/routes/general_routes.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, request, redirect, url_for, render_template, jsonify
+from flask_login import login_required, current_user
+from app.models.models import Store
+from app.extensions import db
+from app.services.assistant_utils import create_dynamic_assistant
+import time
+
 
 general_bp = Blueprint('general', __name__)
 
@@ -23,3 +29,35 @@ def refresh():
 @general_bp.route('/admin_signup', methods=['GET'])
 def show_admin_signup_form():
     return render_template('admin_signup.html')
+
+@general_bp.route('/account_settings')
+def account_settings():
+    return render_template('account_settings.html')
+
+@general_bp.route('/delete_account', methods=['POST'])
+@login_required
+def delete_account():
+    from app.models.models import User, Store, ChatSession, ChatMessage
+    from flask_login import logout_user
+
+    try:
+        user = current_user
+
+        # Delete chat messages
+        ChatMessage.query.filter_by(user_id=user.user_id).delete()
+
+        # Delete chat sessions
+        ChatSession.query.filter_by(user_id=user.user_id).delete()
+
+        # Delete store (if any)
+        Store.query.filter_by(user_id=user.user_id).delete()
+
+        # Now delete the user
+        db.session.delete(user)
+
+        db.session.commit()
+        logout_user()
+        return redirect('/')
+    except Exception as e:
+        db.session.rollback()
+        return render_template("account_settings.html", message=f"Error deleting account: {str(e)}", username=user.username, email=user.email)
